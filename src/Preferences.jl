99ccf251a4208962207a9c1f9fb61fe4fcd80029
Elias Bouacida <elias@bouacida.fr>
2019-04-18 17:27:03 +0200

    Another fix

--- src/Preferences.jl
+++ src/Preferences.jl
@@ -78,7 +78,7 @@ function revealedpreferencesweighted(cf::ChoiceFunction{Int}; n::Int = 0)
         end
     end
     @assert !(sum(weights(result)) == n) "The weights in the preference graph do not add up to $n. PROBLEM."    
-    result.weights = result.weights ./ nchoices
+    result.weights = result.weights ./ n
     return result
 end
 
