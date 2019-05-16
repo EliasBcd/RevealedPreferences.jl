759203233fb5d0ad8580f7e0fc4b15414b8f40e8
Elias Bouacida <19273615+AsileBcd@users.noreply.github.com>
2019-05-16 15:20:22 +0200

    Fix typo

--- src/RevealedPreferences.jl
+++ src/RevealedPreferences.jl
@@ -6,7 +6,7 @@ using DataFrames: DataFrame
 
 import GLPK
 
-export ChoiceFunction, ChoiceCorrespondence, WeightedDiGraph, weights, digraph, setoflaternatives, revealedpreferences, strictrevealedpreferences, indifferencerevealedpreferences, revealedpreferencesweighted, weakrevealedpreferences, transitivecore, strictUCR,  allchoicesets, allcombinationchoicesets, cyclesbylength, HMI, swapindex, optimalset, isWARP, isWARNI, isoutcast, isFAs, isFP, isOO, isalpha, isbeta, isgamma, isdelta, isJLF, isFA
+export ChoiceFunction, ChoiceCorrespondence, WeightedDiGraph, weights, digraph, setoflaternatives, revealedpreferences, strictrevealedpreferences, indifferencerevealedpreferences, revealedpreferencesweighted, weakstrictrevealedpreferences, transitivecore, strictUCR,  allchoicesets, allcombinationchoicesets, cyclesbylength, HMI, swapindex, optimalset, isWARP, isWARNI, isoutcast, isFAs, isFP, isOO, isalpha, isbeta, isgamma, isdelta, isJLF, isFA
 
 include("Preferences.jl")
 include("Rationality/Indices.jl")
