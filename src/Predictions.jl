ea5f973feec4ca12b10f885307524cc0cfeca23b
Elias Bouacida <elias@bouacida.fr>
2019-10-29 14:08:32 +0000

    Add the computation of the optimal set for predictions.

--- src/Predictions.jl
+++ src/Predictions.jl
@@ -2,7 +2,7 @@
 ```optimalset(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}```
 
 Look for all the maximal elements of the DiGraph `dg` in `set`.
-Return the number of alternatives that are admissible.
+Return the set of all maximal elements.
 
 # Arguments
 
@@ -10,14 +10,40 @@ Return the number of alternatives that are admissible.
 - `set`, a set of the same type than the DiGraph.
 """
 function optimalset(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}
+    result = Vector{T}()
+    for t in set
+        copyset = copy(set)
+        s = pop!(copyset)
+        while !has_edge(dg, s, t) & !isempty(copyset)
+            s = pop!(copyset)
+        end
+        if isempty(copyset) & !has_edge(dg, s, t)
+            push!(result, t)
+        end
+    end
+    return result
+end
+
+"""
+```optimalsetsize(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}```
+
+Look for all the maximal elements of the DiGraph `dg` in `set`.
+Return the number of alternatives that are admissible.
+
+# Arguments
+
+- `dg`, a DiGraph;
+- `set`, a set of the same type than the DiGraph.
+"""
+function optimalsetsize(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}
     result = 0
     for t in set
         copyset = copy(set)
         s = pop!(copyset)
-        while !has_edge(dg, s, t) && !isempty(copyset)
+        while !has_edge(dg, s, t) & !isempty(copyset)
             s = pop!(copyset)
         end
-        if isempty(copyset) && !has_edge(dg, s, t)
+        if isempty(copyset) & !has_edge(dg, s, t)
             result += 1
         end
     end
