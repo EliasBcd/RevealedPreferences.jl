31aa7dd1280116e4c69875fd80e83604bc8871db
Elias Bouacida <elias@bouacida.fr>
2019-10-29 15:14:49 +0000

    Remove an redundant function.

--- src/Predictions.jl
+++ src/Predictions.jl
@@ -23,29 +23,3 @@ function optimalset(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}
     end
     return result
 end
-
-"""
-```optimalsetsize(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}```
-
-Look for all the maximal elements of the DiGraph `dg` in `set`.
-Return the number of alternatives that are admissible.
-
-# Arguments
-
-- `dg`, a DiGraph;
-- `set`, a set of the same type than the DiGraph.
-"""
-function optimalsetsize(dg::DiGraph{T}, set::Vector{T}) where {T<:Int}
-    result = 0
-    for t in set
-        copyset = copy(set)
-        s = pop!(copyset)
-        while !has_edge(dg, s, t) & !isempty(copyset)
-            s = pop!(copyset)
-        end
-        if isempty(copyset) & !has_edge(dg, s, t)
-            result += 1
-        end
-    end
-    return result
-end
