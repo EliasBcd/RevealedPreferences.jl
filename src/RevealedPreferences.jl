ed40a46b641cf78c3d820e9d9340a47e38c72299
Elias Bouacida <elias@bouacida.fr>
2019-12-30 10:59:27 +0100

    Type corrected

--- src/RevealedPreferences.jl
+++ src/RevealedPreferences.jl
@@ -7,7 +7,7 @@ using DataFrames: DataFrame
 import GLPK
 import StatsBase.mean
 
-export ChoiceFunction, ChoiceCorrespondence, WeightedDiGraph, weights, digraph, setoflaternatives, overlap, revealedpreferences, strictrevealedpreferences, indifferencerevealedpreferences, revealedpreferencesweighted, weakstrictrevealedpreferences, transitivecore, strictUCR,  allchoicesets, allcombinationchoicesets, cyclesbylength, HMI, swapindex, optimalset, isWARP, isWARNI, isoutcast, isFAs, isFP, isOO, isalpha, isbeta, isgamma, isdelta, isJLF, isFA, isacyclic, fixedpointpreferences, Selten, edgedegree, cyclieswosubcycles!, numbercycleswithoutsubcycles!
+export ChoiceFunction, ChoiceCorrespondence, WeightedDiGraph, weights, digraph, setoflaternatives, overlap, revealedpreferences, strictrevealedpreferences, indifferencerevealedpreferences, revealedpreferencesweighted, weakstrictrevealedpreferences, transitivecore, strictUCR,  allchoicesets, allcombinationchoicesets, cyclesbylength, HMI, swapindex, optimalset, isWARP, isWARNI, isoutcast, isFAs, isFP, isOO, isalpha, isbeta, isgamma, isdelta, isJLF, isFA, isacyclic, fixedpointpreferences, Selten, edgedegree, cyclieswosubcycles!, numbercycleswosubcycles!
 
 include("Preferences.jl")
 include("Rationality/Indices.jl")
