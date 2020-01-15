dcab42c7374b26dcb8e10e2a2e297d5645d5555d
Elias Bouacida <elias@bouacida.fr>
2020-01-15 16:45:26 +0100

    Add function missingedgesdegree to the code

--- src/Degree.jl
+++ src/Degree.jl
@@ -1,5 +1,5 @@
 """
-```edgedegree(dg::DiGraph{T}, e::Edge{T}, f1, f2) where T <: Int```
+```edgedegree(dg::Union{Graph{T}, DiGraph{T}}, e::Edge{T}, f1, f2) where T <: Int```
 
 Compute the relation between in degree and out degree for a given vertice using function `f1`.
 Aggregate the source and the destination degrees of an edge using function `f2`.
@@ -11,7 +11,7 @@ Aggregate the source and the destination degrees of an edge using function `f2`.
 - `f1`, the function used within a vertex;
 - `f2`, the function used within an edge.
 """
-function edgedegree(dg::DiGraph{T}, e::Edge{T}, f1, f2) where T <: Int
+function edgedegree(dg::Union{Graph{T}, DiGraph{T}}, e::Edge{T}, f1, f2) where T <: Int
     res = Vector{Float64}()
     for x in [src(e), dst(e)]
         if f1 == /
@@ -24,7 +24,7 @@ function edgedegree(dg::DiGraph{T}, e::Edge{T}, f1, f2) where T <: Int
 end
 
 """
-```edgedegree(dg::DiGraph{T}, elist::Vector{Edge{T}}, f1 = -, f2 = mean, f3 = mean) where T <: Int```
+```edgesdegree(dg::Union{Graph{T}, DiGraph{T}}, elist::Vector{Edge{T}}, f1 = -, f2 = mean, f3 = mean) where T <: Int```
 
 Compute the relation between in degree and out degree for a given vertice using function `f1`.
 Aggregate the source and the destination degrees of an edge using function `f2`.
@@ -33,15 +33,37 @@ Aggregaste over all edges using function `f3.`
 # Arguments
 
 - `dg`, the digraph on which we compute the degrees;
-- `elist`, the list of edges to consider.
+- `elist`, the list of edges to consider;
 - `f1`, the function used within a vertex;
 - `f2`, the function used within an edge;
-- `f3`, the function used to aggregate between edges;
+- `f3`, the function used to aggregate between edges.
 """
-function edgedegree(dg::DiGraph{T}, elist::Vector{Edge{T}}, f1 = -, f2 = mean, f3 = mean) where T <: Int
+function edgesdegree(dg::Union{Graph{T}, DiGraph{T}}, elist::Vector{Edge{T}}, f1 = -, f2 = mean, f3 = mean) where T <: Int
     res = Vector{Float64}()
     for e in elist
         push!(res, edgedegree(dg, e, f1, f2))
     end
     return f3(res)
+end
+
+"""
+```missingedgesdegree(dg::Union{Graph{T}, DiGraph{T}},f1 = -, f2 = mean, f3 = mean) where T <: Int```
+
+Aggregate the relation between indegree and outdegree of missing edges compared to a complete graph of the same size.
+Compute the relation between in degree and out degree for a given vertice using function `f1`.
+Aggregate the source and the destination degrees of an edge using function `f2`.
+Aggregaste over all edges using function `f3.`
+
+# Arguments
+
+- `dg`, the digraph on which we compute the degrees;
+- `f1`, the function used within a vertex;
+- `f2`, the function used within an edge;
+- `f3`, the function used to aggregate between edges;
+"""
+function missingedgesdegree(dg::Union{Graph{T}, DiGraph{T}},f1 = -, f2 = mean, f3 = mean) where T <: Int
+    g = Graph(dg)
+    cg = complete_graph(nv(g))
+    diffg = difference(cg, g)
+    return edgesdegree(dg, collect(edges(diffg)), f1, f2, f3)
 end
