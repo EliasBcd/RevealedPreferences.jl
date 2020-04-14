c8faa709db9376268017737ea181afc71f0a7d66
Elias Bouacida <elias@bouacida.fr>
2020-04-14 15:21:20 +0200

    Test

--- test/tests.jl
+++ test/tests.jl
@@ -3,4 +3,4 @@ using RevealedPreferences
 
 graph_size = 4
 
-wdg = WeightedDigraph(complete_digraph(graph_size), ones(Int, graph_size, graph_size))
+wdg = WeightedDigraph(complete_digraph(graph_size), ones(graph_size, graph_size))
